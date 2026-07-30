a2864171cd85dac04f1d88c48abaaf18bbc28372
jaejaeji <wogurji@gmail.com>
2026-07-30 18:00:03 -0400

    Add files via upload

---
+++ 01 WhopLab_Fight/script.js
@@ -0,0 +1,5 @@
+function fight() {
+    document.getElementById("p1_healthbar").style.width = "70%";
+    document.getElementById("p2_healthbar").style.width = "35%";
+    document.getElementById("fight_start").textContent = "Fight!";
+}
